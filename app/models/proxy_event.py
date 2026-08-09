31940621d3ce6d32b0d16a61c75192a95f25dd23
softk1t <nazar12522@gmail.com>
2026-08-09 16:36:47 +0200

    ruff fiux

--- app/models/proxy_event.py
+++ app/models/proxy_event.py
@@ -1,7 +1,7 @@
 import uuid
 from datetime import datetime
 
-from sqlalchemy import DateTime, ForeignKey, String, func, BigInteger, Identity
+from sqlalchemy import BigInteger, DateTime, ForeignKey, Identity, String, func
 from sqlalchemy.dialects.postgresql import JSONB, UUID
 from sqlalchemy.orm import Mapped, mapped_column
 
